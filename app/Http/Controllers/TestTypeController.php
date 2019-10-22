2232fef015b036ebe27157344d8536544557bd26
Muhammad Mahedi Hasan <mahadih727@gmail.com>
2019-10-23 00:05:56 +0600

    ..

--- app/Http/Controllers/TestTypeController.php
+++ app/Http/Controllers/TestTypeController.php
@@ -136,6 +136,7 @@ class TestTypeController extends Controller
      */
     public function destroy($id)
     {
-        //
+      $deleted=TestTypeModel::findOrFail($id)->delete();
+      return $deleted ? response()->json(['status'=>200]) : response()->json(['status'=>400]) ;
     }
 }
